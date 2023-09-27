63cc1e0f5ce3f5cf41482de5bc10be8b8e665e08
Maximeuuu <129526478+Maximeuuu@users.noreply.github.com>
2023-09-27 16:39:45 +0200

    Update JAVAC.sh

--- bash/Alias/JAVAC.sh
+++ bash/Alias/JAVAC.sh
@@ -2,6 +2,25 @@
 
 #compile tous les fichiers.java
 
+if [ $# -eq 1 ]; then
+	rep="$1"
+else
+	rep="."
+fi
+
+lstJava=$(ls $rep | grep '.java')
+
+if [ -z "$lstJava" ]; then
+	echo "Aucun fichier .java trouvé."
+	exit 1
+fi
+
 clear
 
-javac *.java
+for java in $lstJava; do
+	echo "----- Compilation de $java -----"
+	javac $java
+done
+
+echo 
+
