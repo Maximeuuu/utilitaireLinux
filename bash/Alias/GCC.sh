c7d084369991d4c4c6880202e3e8b4a2d66e0e95
Maximeuuu <129526478+Maximeuuu@users.noreply.github.com>
2023-11-28 09:26:22 +0100

    Update GCC.sh

--- bash/Alias/GCC.sh
+++ bash/Alias/GCC.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
-# Compile le fichier passe en parametre
+# Vérifie si un fichier C est passé en paramètre
 if [ "$#" -eq "1" ]; then
-	gcc "$1.c" -o "$1.out"
+
+	# Récupère le nom du fichier sans l'extension s'il en a une
+	filename=$(basename -- "$1")
+	extension="${filename##*.}"
+	filename="${filename%.*}"
+
+	# Compile le fichier
+	if [ "$extension" == "c" ]; then
+		gcc "$1" -o "$filename.out"
+		if [ $? -eq 0 ] && [ -e "$filename.out" ]; then
+			echo "Compilation réussie. Exécutable : $filename.out"
+		else
+			echo "Erreur de compilation."
+		fi
+
+	else
+		echo "Extension de fichier incorrecte. Utilisez un fichier avec l'extension '.c'."
+	fi
+
+else
+	echo "Usage: $0 fichier.c"
 fi
